44ee0ee58d6a28577ffabd52f11a118155f64a82
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-12-09 13:58:53 +0330

    Jackson Ignore Properties On Marshaling

--- jackson-ignore-properties-on-marshalling/src/test/java/com/javastudio/tutorial/json/dto/ProductMarshalingTest.java
+++ jackson-ignore-properties-on-marshalling/src/test/java/com/javastudio/tutorial/json/dto/ProductMarshalingTest.java
@@ -2,12 +2,14 @@ package com.javastudio.tutorial.json.dto;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class ProductMarshalingTest {
 
@@ -21,11 +23,19 @@ class ProductMarshalingTest {
     }
 
     @Test
-    void whenMarshalingProduct_thenItShouldContainsName() throws JsonProcessingException {
+    void whenMarshalingProduct_thenItShouldContainsNameAndIgnoreWeight() throws JsonProcessingException {
         Product product = new Product();
         product.setName("Nail");
         product.setWeight(10);
-        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(product);
+        // String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(product);
+        String json = mapper.writeValueAsString(product);
         LOGGER.info(json);
+
+        Pattern pattern = Pattern.compile("name\"\\s*:\"Nail", Pattern.CASE_INSENSITIVE);
+        Matcher matcher=pattern.matcher(json);
+        LOGGER.info("{}", matcher.find());
+
+        Assertions.assertThat(json).containsPattern("name\"\\s*:\"Nail");
+        Assertions.assertThat(json).doesNotContainPattern("weight");
     }
 }
